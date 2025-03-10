338cdf59a25163c1745a376f673cfc22f5c38d02
Miguel Gomez <bydemon101@gmail.com>
2025-03-10 13:11:29 -0500

    App Gestor 2.9

--- task-management-app/backend/routes/userRoutes.js
+++ task-management-app/backend/routes/userRoutes.js
@@ -71,6 +71,13 @@ router.post('/register', [
   }
 });
 
+router.options('/login', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://app-task-chi.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.sendStatus(200);
+});
+
 // @route   POST /api/users/login
 // @desc    Authenticate user & get token
 // @access  Public
